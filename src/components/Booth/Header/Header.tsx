35aca3eef4e2e923030abd6fc0b468453f536554
ej <liojung518@gmail.com>
2025-01-07 14:53:26 +0900

    [#23] asset : 이미지 수정 및 import 부스 수정정

--- src/components/Booth/Header/Header.tsx
+++ src/components/Booth/Header/Header.tsx
@@ -1,27 +1,18 @@
 import React, { useState } from 'react';
 import * as S from './Styles';
-import { ReactComponent as Ateway } from 'assets/icon/booth/ateway.svg';
-import { ReactComponent as D } from 'assets/icon/booth/D.svg';
-import { ReactComponent as G } from 'assets/icon/booth/G.svg';
-import { ReactComponent as G2 } from 'assets/icon/booth/G.svg';
-import { ReactComponent as GDoc } from 'assets/icon/booth/DGoC CUK.svg';
-import { ReactComponent as Ating } from 'assets/icon/booth/ating.svg';
-import { ReactComponent as Ggrey } from 'assets/icon/booth/G grey.svg';
-import { ReactComponent as Dgrey } from 'assets/icon/booth/D grey.svg';
-import { ReactComponent as HeaderLogo } from 'assets/icon/booth/headerLogo.svg';
 
 const Header: React.FC = () => {
     const [selected, setSelected] = useState('G');
 
     return (
         <S.HeaderLayout>
-            {selected === 'G' ? <G /> : <Ggrey onClick={() => setSelected('G')} />}
-            {selected === 'G' && <Ateway  />}
-            {selected === 'D' ? <D /> : <Dgrey onClick={() => setSelected('D')} />}
-            {selected === 'D' && <Ating />}
-            {selected === 'G2' ? <G2 /> : <Ggrey onClick={() => setSelected('G2')} />}
-            {selected === 'G2' && <GDoc />}
-            <HeaderLogo className="headerLogo" />
+            {selected === 'G' ? <S.G /> : <S.GGrey onClick={() => setSelected('G')} />}
+            {selected === 'G' && <S.Ateway  />}
+            {selected === 'D' ? <S.D /> : <S.Dgrey onClick={() => setSelected('D')} />}
+            {selected === 'D' && <S.Ating />}
+            {selected === 'G2' ? <S.G2 /> : <S.GGrey onClick={() => setSelected('G2')} />}
+            {selected === 'G2' && <S.DGoc />}
+            <S.HeaderLogo className="headerLogo" />
         </S.HeaderLayout>
     );
 }
--- src/components/Booth/Header/Styles.ts
+++ src/components/Booth/Header/Styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import * as I from '../../../assets/icon';
 
 export const HeaderLayout = styled.div`
   display: flex;
@@ -15,6 +16,41 @@ export const HeaderLayout = styled.div`
     margin-bottom: -2%; 
     margin-left: -3%;
   }
+`;
+
+export const G = styled(I.G)`
+  cursor: pointer;
+`;
+
+export const GGrey = styled(I.GGrey)`
+  cursor: pointer;
+`;
+
+export const D = styled(I.D)`
+  cursor: pointer;
+`;
+
+export const Dgrey = styled(I.DGrey)`
+  cursor: pointer;
+`;
+
+export const G2 = styled(I.G)`
+  cursor: pointer;
+`;
+
+export const Ateway = styled(I.ateway)`
+`;
+
+export const DGoc = styled(I.DGoCCUK)`
+`;
+
+export const HeaderLogo = styled(I.HeaderLogo)`
+`;
+
+export const Ating = styled(I.Ating)`
+`;
+
+export const DGoCCUK = styled(I.DGoCCUK)`
 `;
 
 
