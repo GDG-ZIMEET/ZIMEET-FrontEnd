e5a9a5d0287cdff0496cf923d9c443a3688ac997
oyejin0925 <142577684+oyejin0925@users.noreply.github.com>
2025-01-08 01:01:13 +0900

    [#22] feat: 네비게이션바 제작

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,19 +1,64 @@
 import React from 'react';
-// ...existing code...
+import * as I from '../../assets/Icons.ts'; 
+import * as S from './Styles';
+import { useNavigate, useLocation } from 'react-router-dom'; 
 
 const NavigationBar = () => {
-    // ...existing code...
+    const navigate = useNavigate();
+    const location = useLocation();
 
-    const handleClick = () => {
-        console.log('임시 파일일');
+    const iconMapping = {
+        '/': {
+            selected: <I.Home />,
+            default: <I.HomeWhite />,
+        },
+        '/meeting22': {
+            selected: <I.MeetingHeart />,
+            default: <I.Meeting />,
+        },
+        '/chatting': {
+            selected: <I.Chatting />,
+            default: <I.Chatting />,
+        },
+        '/myinformation': {
+            selected: <I.MyInformation />,
+            default: <I.MyInformation />,
+        },
+    };
+
+    const getIcon = (path) => {
+        return location.pathname === path
+            ? iconMapping[path]?.selected
+            : iconMapping[path]?.default;
     };
 
     return (
-        <nav>
-            // ...existing code...
-            <button onClick={handleClick}>New Button</button>
-            // ...existing code...
-        </nav>
+        <S.NavBarContainer>
+            <S.NavItem onClick={() => navigate('/')}>
+                <S.IconWrapper>
+                    {getIcon('/')}
+                </S.IconWrapper>
+                홈
+            </S.NavItem>
+            <S.NavItem onClick={() => navigate('/meeting22')}>
+                <S.IconWrapper style={{ margin: location.pathname === '/meeting22' ? '5px' : '6px' }}>
+                    {getIcon('/meeting22')}
+                </S.IconWrapper>
+                미팅
+            </S.NavItem>
+            <S.NavItem onClick={() => navigate('/chatting')}>
+                <S.IconWrapper style={{ margin: '1px' }}>
+                    {getIcon('/chatting')}
+                </S.IconWrapper>
+                채팅
+            </S.NavItem>
+            <S.NavItem>
+                <S.IconWrapper style={{ margin: '5px' }}>
+                    {getIcon('/myinformation')}
+                </S.IconWrapper>
+                내정보
+            </S.NavItem>
+        </S.NavBarContainer>
     );
 };
 
---
+++ src/components/NavigationBar/Styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const NavBarContainer = styled.nav`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 70px;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-top: 1px solid #f0f0f0;
+  z-index: 10;
+`;
+
+export const NavItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  font-size: 12px;
+  color: #151515;
+  text-align: center;
+  cursor: pointer;
+  padding: 5px 0;
+`;
+
+export const IconWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+`;
