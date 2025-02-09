9d56f7038cf7c4095f2ad925244d24b033be41f1
ej <liojung518@gmail.com>
2025-02-09 17:57:56 +0900

    [#142] design: 팝업 ui 수정

---
+++ src/data/NonLoginData.ts
@@ -0,0 +1,132 @@
+
+export const NonLoginDataTwoToTwo = [
+  {
+    "teamId": 8,
+    "emoji": ["Cat", "Bear"],
+    "name": "야구보러갈래요?",
+    "verification": 0,
+    "major": ["컴공", "인공"],
+    "age": 22.0,
+    "music": ["인디밴드", "K-POP"]
+  },
+  {
+    "teamId": 7,
+    "emoji": ["Beer", "Butterfly"],
+    "name": "직전학기A+",
+    "verification": 1,
+    "major": ["법학"],
+    "age": 23.5,
+    "music": ["발라드", "팝송"]
+  },
+  {
+    "teamId": 3,
+    "emoji": ["Camera", "Blossom"],
+    "name": "밴드스팅",
+    "verification": 0,
+    "major": ["음악"],
+    "age": 21.0,
+    "music": ["락밴드", "밴드"]
+  },
+  {
+    "teamId": 4,
+    "emoji": ["cheese", "Clover"],
+    "name": "역곡에서놀아요!",
+    "verification": 1,
+    "major": ["심리"],
+    "age": 23.0,
+    "music": ["발라드", "K-POP"]
+  },
+  {
+    "teamId": 5,
+    "emoji": ["Donut", "ExcitedFace"],
+    "name": "지디지",
+    "verification": 0,
+    "major": ["컴공", "통"],
+    "age": 23.0,
+    "music": ["발라드", "밴드"]
+  },
+  {
+    "teamId": 6,
+    "emoji": ["Fox", "Fire"],
+    "name": "달달한 데이트",
+    "verification": 0,
+    "major": ["철학"],
+    "age": 21.0,
+    "music": ["팝송", "K-POP"]
+  },
+  {
+    "teamId": 10,
+    "emoji": ["Frog", "Fries"],
+    "name": "지디",
+    "verification": 0,
+    "major": ["철학"],
+    "age": 21.0,
+    "music": ["발라드", "K-POP"]
+  }
+];
+
+export const NonLoginDataThreeToThree = [
+  {
+    "teamId": 8,
+    "emoji": ["Cat", "Beer", "Bear"],
+    "name": "야구보러갈래요?",
+    "verification": 0,
+    "major": ["컴공", "인공", "데사"],
+    "age": 22.0,
+    "music": ["발라드", "인디밴드", "k-pop"]
+  },
+  {
+    "teamId": 7,
+    "emoji": ["Beer", "Cat", "Butterfly"],
+    "name": "직전학기A+",
+    "verification": 1,
+    "major": ["법학"],
+    "age": 23.5,
+    "music": ["발라드"]
+  },
+  {
+    "teamId": 3,
+    "emoji": ["Camera", "Beer", "Blossom"],
+    "name": "고래고래",
+    "verification": 0,
+    "major": ["미콘"],
+    "age": 21.0,
+    "music": ["K-POP", "발라드", "pop"]
+  },
+  {
+    "teamId": 4,
+    "emoji": ["cheese", "Beer", "Clover"],
+    "name": "커피한잔할래요",
+    "verification": 1,
+    "major": ["음악"],
+    "age": 23.0,
+    "music": ["pop", "클래식", "pop"]
+  },
+  {
+    "teamId": 5,
+    "emoji": ["Donut", "Beer", "ExcitedFace"],
+    "name": "긱사팟",
+    "verification": 0,
+    "major": ["심리", "프문", "영문"],
+    "age": 23.0,
+    "music": ["발라드", "인디밴드", "pop"]
+  },
+  {
+    "teamId": 6,
+    "emoji": ["Fox", "Beer", "Fire"],
+    "name": "늘품제에 만나요",
+    "verification": 0,
+    "major": ["경제"],
+    "age": 21.0,
+    "music": ["밴드", "pop", "J-POP"]
+  },
+  {
+    "teamId": 10,
+    "emoji": ["Frog", "Beer", "Fries"],
+    "name": "지디",
+    "verification": 0,
+    "major": ["철학"],
+    "age": 21.0,
+    "music": ["발라드"]
+  }
+];
--- src/pages/Meeting22/Styles.ts
+++ src/pages/Meeting22/Styles.ts
@@ -1,19 +1,12 @@
 import styled from 'styled-components';
 
-export const Meeting22Layout = styled.div<{ $isLoggedIn: boolean }>`
+export const Meeting22Layout = styled.div`
   padding: 6% 0% 0;
   background-color: #FFFFFF;
   top: 0;
   height: 100%;
   display: flex;
   flex-direction: column;
-
-  ${({ $isLoggedIn }) =>
-    !$isLoggedIn &&
-    `
-    pointer-events: none;  
-    filter: blur(5px); 
-  `}
 `;
 
 
