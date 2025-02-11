c0b0c99965d3dfbad19292f4e203f1a288579a48
SeyeonJang <seye0n.bliss@gmail.com>
2025-02-11 22:13:50 +0900

    [#103] feat: 하이를 받은 팀에서 모달을 열고 닫는 로직 추가 - 하이 수락, 수락 완료 모달 - 하이 거절, 거절 완료 모달

--- src/pages/Chatting/ReceivedHiTeam/ReceivedHiTeam.tsx
+++ src/pages/Chatting/ReceivedHiTeam/ReceivedHiTeam.tsx
@@ -5,11 +5,52 @@ import Profiles from "../../../components/TeamIntro/Profile/ProfileData";
 import Header from "../../../components/TeamIntro/Header/Header";
 import MyProfile from "../../../components/TeamIntro/Profile/MyProfile";
 import NavigationBar from "../../../components/NavigationBar/NavigationBar";
+import AcceptHiModal from "../../../components/Chatting/ReceiveHi/Modal/AcceptHiModal/AcceptHiModal";
+import AcceptedHiModal from "../../../components/Chatting/ReceiveHi/Modal/AcceptedHiModal/AcceptedHiModal";
+import RefuseHiModal from "../../../components/Chatting/ReceiveHi/Modal/RefuseHiModal/RefuseHiModal";
+import RefusedHiModal from "../../../components/Chatting/ReceiveHi/Modal/RefusedHiModal/RefusedHiModal";
 
 const ReceivedHiTeam: React.FC = () => {
+    const navigate = useNavigate();
     const [isPremium, setIsPremium] = useState(true); // TODO : API 연결 후 RecoilState로 변경
+    const [isAcceptModalOpen, setIsAcceptModalOpen] = useState(false);
+    const [isRefuseModalOpen, setIsRefuseModalOpen] = useState(false);
+    const [isAcceptedModalOpen, setIsAcceptedModalOpen] = useState(false);
+    const [isRefusedModalOpen, setIsRefusedModalOpen] = useState(false);
 
     const profileData = Profiles.filter(profile => profile.isMe).slice(0, 2);
+
+    const openAcceptModal = () => {
+        setIsAcceptModalOpen(true);
+    };
+    const closeAcceptModal = () => {
+        setIsAcceptModalOpen(false);
+    };
+
+    const openAcceptedModal = () => {
+        setIsAcceptModalOpen(false);
+        setIsAcceptedModalOpen(true);
+    };
+    const closeAcceptedModal = () => {
+        setIsAcceptedModalOpen(false);
+        navigate('/chattingInventory');
+    };
+
+    const openRefuseModal = () => {
+        setIsRefuseModalOpen(true);
+    };
+    const closeRefuseModal = () => {
+        setIsRefuseModalOpen(false);
+    };
+
+    const openRefusedModal = () => {
+        setIsRefuseModalOpen(false);
+        setIsRefusedModalOpen(true);
+    };
+    const closeRefusedModal = () => {
+        setIsRefusedModalOpen(false);
+        navigate('/receiveHi');
+    };
 
     return (
         <S.Container>
@@ -21,10 +62,15 @@ const ReceivedHiTeam: React.FC = () => {
                     isPremium={isPremium} />
             ))}
             <S.ButtonWrapper>
-                <S.RefuseButton>거절</S.RefuseButton>
-                <S.Button>하이 수락하기</S.Button>
+                <S.RefuseButton onClick={openRefuseModal}>거절</S.RefuseButton>
+                <S.Button onClick={openAcceptModal}>하이 수락하기</S.Button>
             </S.ButtonWrapper>
             <NavigationBar />
+
+            {isAcceptModalOpen && <AcceptHiModal onClose={closeAcceptModal} onConfirm={openAcceptedModal} teamName={'팀명예시'}/>}
+            {isAcceptedModalOpen && <AcceptedHiModal onClose={closeAcceptedModal}/>}
+            {isRefuseModalOpen && <RefuseHiModal onClose={closeRefuseModal} onConfirm={openRefusedModal} teamName={'팀명예시'}/>}
+            {isRefusedModalOpen && <RefusedHiModal onClose={closeRefusedModal}/>}
         </S.Container>
     );
 };
