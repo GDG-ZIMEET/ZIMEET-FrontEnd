5a2349399174e93e48caa169fad610a435b38d24
조은정/이현 <165488194+E-hyeon@users.noreply.github.com>
2025-02-14 18:21:31 +0900

    [dev] 백엔드에게 보내주는 데이터 정리

---
+++ src/data/SignUpData.ts
@@ -0,0 +1,82 @@
+//학년
+export const gradeOptions = ["FIRST", "SECOND", "THIRD", "FOURTH"];
+export const gradeDisplayOptions = [1, 2, 3, 4];
+
+//학과
+export const majorOptions = [
+    "국어국문학과", "철학과", "국사학과", "아동학과",
+    "영어영문학과", "중어중문학과", "일어일문학과", "불어불문학과",
+    "음악과", "신학과",
+    "사회복지학과", "심리학과", "사회학과", "특수교육과",
+    "경영학과", "회계학과",
+    "국제학과", "법학과", "경제학과", "행정학과",
+    "글로벌경영학부",
+    "화학과", "수학과", "물리학과",
+    "공간디자인소비자학과", "의류학과", "식품영양학과",
+    "의생명과학과", "약학과",
+    "간호학과", "의학과",
+    "컴퓨터정보공학과", "미디어기술콘텐츠학과", "정보통신전자공학과",
+    "바이오메디컬화학공학과", "에너지환경공학과", "생명공학과",
+    "인공지능학과", "데이터사이언스학과", "바이오메디컬소프트웨어학과"
+  ];
+  
+//mbti
+export const mbtiOptions = [
+    'ISTJ', 'ISFJ', 'ESTJ', 'ESFJ', 
+    'ISTP', 'ISFP', 'ESTP', 'ESFP', 
+    'INTJ', 'INTP', 'ENTJ', 'ENTP', 
+    'INFJ', 'INFP', 'ENFJ', 'ENFP'
+];
+
+//음악
+export const musicMap: { [key: string]: string } = {
+    'K-POP': 'KPOP',
+    'POP': 'POP',
+    '발라드': 'BALLAD',
+    '힙합': 'HIPHOP',
+    'J-POP': 'JPOP',
+    '재즈': 'JAZZ',
+    '밴드': 'BAND',
+    '클래식': 'CLASSICAL',
+  };
+  
+  export const musics = Object.keys(musicMap);
+  
+  //스타일 
+  export const styleMap: { [key: string]: string } = {
+    '캐주얼': 'CASUAL',
+    '스트릿': 'STREET',
+    '단정': 'NEAT',
+    '빈티지': 'VINTAGE',
+    '힙': 'HEAP',
+    '스포티': 'SPORTY',
+    '포멀': 'FORMAL',
+    '큐티': 'CUTIE',
+  };
+  
+  export const styles = Object.keys(styleMap);
+  
+  //이상형 나이 
+  export const ageMap: { [key: string]: string } = {
+    '연상': 'OLDER',
+    '연하': 'YOUNGER',
+    '동갑': 'SAME',
+    '무관': 'NO_MATTER',
+  };
+  
+  export const ages = Object.keys(ageMap);
+  
+  //이상형 유형
+  export const faceMap: { [key: string]: string } = {
+    '강아지': 'DOG',
+    '고양이': 'CAT',
+    '햄스터': 'HAMSTER',
+    '여우': 'FOX',
+    '곰': 'BEAR',
+    '공룡': 'DINOSAUR',
+    '토끼': 'RABBIT',
+    '늑대': 'WOLF',
+  };
+  
+  export const faces = Object.keys(faceMap);
+  
