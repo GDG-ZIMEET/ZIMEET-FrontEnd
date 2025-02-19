a745c4384189841d746067b082f8c7e08b4ad3c7
조은정/이현 <165488194+E-hyeon@users.noreply.github.com>
2025-02-20 04:30:23 +0900

    [#178] feat: 로그인 세션저장

--- src/recoil/state/authState.ts
+++ src/recoil/state/authState.ts
@@ -5,7 +5,30 @@ interface AuthState {
   refreshToken: string;
 }
 
+
+const getSessionAuthState = (): AuthState | null => {
+  const storedAuth = sessionStorage.getItem('authState');
+  return storedAuth ? JSON.parse(storedAuth) : null;
+};
+
+
+const setSessionAuthState = (auth: AuthState | null) => {
+  if (auth) {
+    sessionStorage.setItem('authState', JSON.stringify(auth));
+  } else {
+    sessionStorage.removeItem('authState');
+  }
+};
+
+
 export const authState = atom<AuthState | null>({
   key: 'authStateType', 
-  default: null,
+  default: getSessionAuthState(), 
+  effects_UNSTABLE: [
+    ({ onSet }) => {
+      onSet((newValue) => {
+        setSessionAuthState(newValue); 
+      });
+    },
+  ],
 });
