9eb75977730e228c72536dd546ecf8af4c28577e
oyejin0925 <142577684+oyejin0925@users.noreply.github.com>
2025-03-03 23:23:09 +0100

    [#215] 로그인 쿠키로 변경

--- src/api/Authentication/useLogin.js
+++ src/api/Authentication/useLogin.js
@@ -1,6 +1,7 @@
 import { useSetRecoilState } from 'recoil';
 import { authState } from 'recoil/state/authState';
 import { publicAxios } from 'api/axiosConfig';
+import Cookies from 'js-cookie'; // js-cookie 사용
 
 const useLogin = () => {
   const setAuthState = useSetRecoilState(authState);
@@ -16,10 +17,11 @@ const useLogin = () => {
 
       const { accessToken, refreshToken } = response.data.data;
       setAuthState({ accessToken, refreshToken });
-      localStorage.setItem('accessToken', accessToken);
-      localStorage.setItem('refreshToken', refreshToken);
-      navigate('/meeting22');
 
+      Cookies.set('accessToken', accessToken, { expires: 7, path: '' });
+      Cookies.set('refreshToken', refreshToken, { expires: 7, path: '' });
+
+      navigate('/meeting22');
     } catch (error) {
       const errorMessage = error.response?.data?.message || '로그인 실패';
       setErrorMessage(errorMessage);
