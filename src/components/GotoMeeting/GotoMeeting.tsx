9fadc024a7e2be1db4714e520f5ea5cc4609cd46
SeyeonJang <seye0n.bliss@gmail.com>
2025-03-18 22:03:44 +0900

    [#245] feat: 홈(부스), 부스상세 화면 track 이벤트 추가

--- src/components/GotoMeeting/GotoMeeting.tsx
+++ src/components/GotoMeeting/GotoMeeting.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as S from './Styles';
+import * as amplitude from '@amplitude/analytics-browser';
 
 const GotoMeeting: React.FC = () => {
   const navigate = useNavigate();
 
   const handleGotoMeetingClick = () => {
-    navigate('/meeting-random');
+    navigate('/meeting22');
+    amplitude.track('[클릭]부스_플로팅버튼');
+    
   };
 
   return (
