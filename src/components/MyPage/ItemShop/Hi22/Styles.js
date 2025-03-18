5ff04a7389fce170d634d237dc4ea168e60c0194
조은정/이현 <165488194+E-hyeon@users.noreply.github.com>
2025-03-18 21:27:29 +0900

    [#246] refacor: 마이페이지 css 수정

--- src/components/MyPage/ItemShop/Hi22/Styles.js
+++ src/components/MyPage/ItemShop/Hi22/Styles.js
@@ -1,47 +1,50 @@
 import styled from 'styled-components';
+import * as I from '../../../../assets/Icons';
 
 export const ItemHi22Container = styled.div`
-  width: 100%; height: 30%;
+  width: 100%; 
+  height: 20%;
   background-color: #f6f5fa;
-  padding: 3%;
+  padding: 4% 4% 4% 4%;
   border-radius: 0.8rem;
-  margin-top: 5%;
+  margin-top: 3%;
 `;
 
 export const Title = styled.p`
   font-weight: 700;
   font-size: 12px;
-  padding: 5% 2%;
+  margin-bottom: 2%;
 `;
 
 export const HiContainer = styled.div`
   display: flex;
   justify-content: space-around;
-  align-items: center;
   gap: 3%; 
   height: 72%;
 `;
 
 export const HiWrpper = styled.div`
-  width: 100%; height: 100%;
+  width: 100%; 
+  flex: 1;
   align-items: center;
   text-align: center;
   border-radius: 0.8rem;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
-  margin-bottom: 2%;
 `;
 
 export const HiQuantity = styled.div`
   height: 65%;
   background-color: black;
-  padding: 11%;
-  border-top-left-radius: 0.8rem; border-top-right-radius: 0.8rem;
+  padding: 10% 10% 10% 10%;
+  border-top-left-radius: 0.8rem; 
+  border-top-right-radius: 0.8rem;
 `;
 
 export const HiQuantityText = styled.p`
   color: white;
   font-size: 12px;
   font-weight: 700;
+  margin-bottom: 2%;
 `;
 
 export const HiPrice = styled.div`
@@ -63,4 +66,46 @@ export const DiscountPrice = styled.p`
   font-weight: 700;
   color: #eb4335;
   padding: 2%;
+`;
+
+export const Hi1 = styled(I.Hi1)`
+  width: 63%;
+  height: 90%;
+`;
+
+export const Hi3 = styled(I.Hi3)`
+  width: 100%;
+  height: 70%;
+  margin-top: 5%;
+`;
+
+export const Hi10 = styled(I.Hi10)`
+  width: 90%;
+  height: 85%;
+  margin-top: 2%;
+`;
+
+export const T1 = styled(I.Ticket1)`
+  width: 63%;
+  height: 90%;
+`;
+
+export const T3 = styled(I.Ticket3)`
+  width: 100%;
+  height: 70%;
+  margin-top: 5%;
+`;
+
+export const T8 = styled(I.Ticket8)`
+  width: 90%;
+  height: 85%;
+  margin-top: 2%;
+`;
+export const TicketsContainer = styled.div`
+  width: 100%; 
+  height: 20%;
+  background-color: #f6f5fa;
+  padding: 5% 5% 3% 5%;
+  border-radius: 0.8rem;
+  margin-top: 3%;
 `;
--- src/components/MyPage/ItemShop/Main/Styles.js
+++ src/components/MyPage/ItemShop/Main/Styles.js
@@ -1,17 +1,16 @@
 import styled from 'styled-components';
 
-export const ItemShopContainer = styled.div`
+export const ItemShopLayout = styled.div`
   width: 90%;
-  font-family: Pretendard;
+  margin: 3% auto 0 auto;
+  display: flex;
+  flex-direction: column;
+  font-family: Pretendard, sans-serif;
+
+  @media (max-width: 768px) {
+  @supports (-webkit-touch-callout: none) {
+      height: 17%;
+    }
+  }
 `;
-export const Title = styled.p`
-    font-family: Pretendard;
-    font-size: 15px;
-    font-weight: 700;
-    padding: 1% 0;
-`;
-export const Text = styled.p`
-    font-family: Pretendard;
-    font-size: 12px;
-    padding: 1% 0;
-`;
+
