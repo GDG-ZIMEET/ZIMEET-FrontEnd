f47339753f04d4eb25682ff31b4d14602565ccf4
SeyeonJang <seye0n.bliss@gmail.com>
2025-03-19 01:53:13 +0900

    [#245] feat: 미팅 화면 track 이벤트 추가

--- src/components/Meeting22/TypeButton/TypeButton.tsx
+++ src/components/Meeting22/TypeButton/TypeButton.tsx
@@ -1,59 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import * as S from './Styles';
+import { track } from '@amplitude/analytics-browser';
 
 interface TypeButtonProps {
   setSelectedTeamType: (teamType: string) => void;
 }
 
 const TypeButton: React.FC<TypeButtonProps> = ({ setSelectedTeamType }) => {
-  const location = useLocation();
-  const [selectedButton, setSelectedButton] = useState<string>('2to2');
+    const location = useLocation();
+    const [selectedButton, setSelectedButton] = useState<string>('2to2');
 
+    useEffect(() => {
+        track('[접속]미팅_2대2');
+    });
 
-  const handleTypeChange = (teamType: string, button: string, size: number) => {
-    setSelectedButton(button);
-    setSelectedTeamType(teamType); 
-  };
+    const handleTypeChange = (teamType: string, button: string, size: number) => {
+        setSelectedButton(button);
+        setSelectedTeamType(teamType);
 
-  const handleRClick = () => {
-    setSelectedButton('random');
-    setSelectedTeamType('Random');
-  };
+        if (button === '2to2') {
+            track('[클릭]미팅_헤더_2대2버튼');
+            track('[접속]미팅_2대2');
+        } else if (button === '3to3') {
+            track('[클릭]미팅_헤더_3대3버튼');
+            track('[접속]미팅_3대3');
+        }
+    };
 
-  return (
-    <S.TypeLayout>
-      <S.TypeComponent>
-        <S.Twoto2
-          onClick={() => handleTypeChange('TWO_TO_TWO', '2to2', 2)}
-          selected={selectedButton === '2to2'}
-        >
-          2대2
-        </S.Twoto2>
-      </S.TypeComponent>
-      <S.TypeComponent>
-        <S.Threeto3
-          onClick={() => handleTypeChange('THREE_TO_THREE', '3to3', 3)}
-          selected={selectedButton === '3to3'}
-        >
-          3대3
-        </S.Threeto3>
-      </S.TypeComponent>
-      <S.TypeComponent>
-        <S.Random
-          onClick={handleRClick}
-          selected={selectedButton === 'random'}
-        >
-          랜덤
-        </S.Random>
-      </S.TypeComponent>
-      <S.lookComponent>
-        {location.pathname !== '/meeting-random' && (
-          <S.lookword>이성 팀만 보여요👀</S.lookword>
-        )}
-      </S.lookComponent>
-    </S.TypeLayout>
-  );
+    const handleRClick = () => {
+        setSelectedButton('random');
+        setSelectedTeamType('Random');
+
+        track('[클릭]미팅_헤더_랜덤버튼');
+        track('[접속]미팅_랜덤');
+    };
+
+    return (
+        <S.TypeLayout>
+            <S.TypeComponent>
+                <S.Twoto2
+                    onClick={() => handleTypeChange('TWO_TO_TWO', '2to2', 2)}
+                    selected={selectedButton === '2to2'}
+                >
+                    2대2
+                </S.Twoto2>
+            </S.TypeComponent>
+            <S.TypeComponent>
+                <S.Threeto3
+                    onClick={() => handleTypeChange('THREE_TO_THREE', '3to3', 3)}
+                    selected={selectedButton === '3to3'}
+                >
+                    3대3
+                </S.Threeto3>
+            </S.TypeComponent>
+            <S.TypeComponent>
+                <S.Random
+                    onClick={handleRClick}
+                    selected={selectedButton === 'random'}
+                >
+                    랜덤
+                </S.Random>
+            </S.TypeComponent>
+            <S.lookComponent>
+                {location.pathname !== '/meeting-random' && (
+                    <S.lookword>이성 팀만 보여요👀</S.lookword>
+                )}
+            </S.lookComponent>
+        </S.TypeLayout>
+    );
 };
 
 export default TypeButton;
