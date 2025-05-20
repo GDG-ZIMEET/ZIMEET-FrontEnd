0048ec0cdc4edcfeb84918777f02b7cd0fd03f51
SeyeonJang <seye0n.bliss@gmail.com>
2025-05-21 01:25:20 +0900

    [#274] feat: 2대2 미팅 프로필에 알림 설정 버튼 추가

---
+++ src/components/OurTeamIntro/Modal/NotificationButton/NotificationButton.tsx
@@ -0,0 +1,20 @@
+import * as S from './Styles';
+import { useFCM } from '../../../../firebase/useFCM';
+
+const NotificationButton = () => {
+    const { requestNotificationPermission } = useFCM();
+    const isNotificationEnabled = Notification.permission === 'granted';
+
+    if (isNotificationEnabled) return null;
+
+    return (
+        <S.NotificationLayout>
+            <S.NotificationButton onClick={requestNotificationPermission}>
+                <S.NotificationIcon />
+                <S.NotificationText>눌러서 하이 받을 때, 채팅 올 때 알림 받기</S.NotificationText>
+            </S.NotificationButton>
+        </S.NotificationLayout>
+    );
+};
+
+export default NotificationButton; 
