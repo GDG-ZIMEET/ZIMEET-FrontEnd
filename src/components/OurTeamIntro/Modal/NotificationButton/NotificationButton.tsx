71e8c4bd7d7dda259ad635cc4c8490e550b0846a
SeyeonJang <seye0n.bliss@gmail.com>
2025-05-22 01:08:27 +0900

    [#279] fix: 2대2 미팅 프로필 알림설정 로직 구현

--- src/components/OurTeamIntro/Modal/NotificationButton/NotificationButton.tsx
+++ src/components/OurTeamIntro/Modal/NotificationButton/NotificationButton.tsx
@@ -4,12 +4,15 @@ import { useFCM } from '../../../../firebase/useFCM';
 const NotificationButton = () => {
     const { requestNotificationPermission } = useFCM();
     const isNotificationEnabled = Notification.permission === 'granted';
+    const handleNotificationPermission = async () => {
+        await requestNotificationPermission();
+    };
 
     if (isNotificationEnabled) return null;
 
     return (
         <S.NotificationLayout>
-            <S.NotificationButton onClick={requestNotificationPermission}>
+            <S.NotificationButton onClick={handleNotificationPermission}>
                 <S.NotificationIcon />
                 <S.NotificationText>눌러서 하이 받을 때, 채팅 올 때 알림 받기</S.NotificationText>
             </S.NotificationButton>
