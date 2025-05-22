737bb78cec233ea68b2638e19ccf86fb3906b95e
SeyeonJang <seye0n.bliss@gmail.com>
2025-05-22 11:36:59 +0900

    [#287] fix: 받은 하이, 보낸 하이 멘트 수정

--- src/pages/Chatting/SentHi/SentHi.tsx
+++ src/pages/Chatting/SentHi/SentHi.tsx
@@ -17,7 +17,7 @@ const SentHi: React.FC = () => {
       <S.SentHiExplanation>
         <strong>나 또는 우리 팀이 보냈던 하이</strong>
         <br />
-        보낸 하이는 5시간 뒤에 상대방의 받은 하이 목록에서 사라져요.
+        보낸 하이는 12시간 뒤에 상대방의 받은 하이 목록에서 사라져요.
       </S.SentHiExplanation>
       <Teams />
       <NavigationBar />
